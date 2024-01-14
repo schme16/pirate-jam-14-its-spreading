94f296a65ec4ea22ccf2069acbb22b994f30cbe4
Shane Gadsby <schme16@gmail.com>
2024-01-14 17:08:19 +1000

    Sorta working ink balls

---
+++ Assets/data/scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //This is the current state the game is in, e.g. "main menu" "playing" "game over" etc 
+    public string state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/data/scripts/PlayerScript.cs
+++ Assets/data/scripts/PlayerScript.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-	public float movementSpeed = 0.4f;
-	public float jumpSpeed = 1;
+	public float movementSpeed = 0.125f;
+	public float maxSpeed = 2f;
+	public float jumpSpeed = 5;
+	public float minDistBetweenIckBalls = 5;
+	public ParticleSystem ps;
+	public Transform ickBallPrefab;
 
 	private Camera cam;
 	private Rigidbody rb;
@@ -13,17 +18,26 @@ public class PlayerScript : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
+		//Set the camera
 		cam = Camera.main;
+
+		//Set the rigidbody
 		rb = GetComponent<Rigidbody>();
+
+		//Hide the cursor
 		Cursor.visible = false;
+
+		//Lock the cursor to the game window
 		Cursor.lockState = CursorLockMode.Locked;
 	}
 
-	// Update is called once per frame
+	// Physics updates
 	void Update()
 	{
 		//float x = Input.GetAxis("Horizontal");
 		bool jump = Input.GetButtonDown("Jump");
+		bool sneak = Input.GetButtonDown("Sneak");
+		bool dash = Input.GetButtonDown("Dash");
 		float y = Input.GetAxis("Vertical");
 
 		/*if (x != 0)
@@ -32,15 +46,44 @@ public class PlayerScript : MonoBehaviour
 		}*/
 
 		transform.eulerAngles = new Vector3(transform.eulerAngles.x, cam.transform.eulerAngles.y, transform.eulerAngles.z);
-		
+
 		if (y != 0)
 		{
-			rb.AddForce(transform.forward * (movementSpeed * y), ForceMode.VelocityChange);
+			var maxV = transform.forward * (maxSpeed * y);
+			rb.AddForce(maxV - rb.velocity, ForceMode.Force);
 		}
-		
+
 		if (jump)
 		{
 			rb.AddForce(transform.up * (movementSpeed * jumpSpeed), ForceMode.Impulse);
+		}
+
+		if (rb.velocity.magnitude > 1)
+		{
+			//Debug.Log(rb.velocity.magnitude);
+		}
+	}
+	
+	void OnDrawGizmosSelected()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, minDistBetweenIckBalls);
+    }
+	private void OnParticleCollision(GameObject other)
+	{
+		Collider[] hitColliders = Physics.OverlapSphere(transform.position, minDistBetweenIckBalls, LayerMask.GetMask("ick-ball"), QueryTriggerInteraction.Collide);
+		bool hasHit = false;
+		foreach (var hitCollider in hitColliders)
+		{
+			hasHit = true;
+			Debug.Log(hitCollider.name);
+		}
+
+		if (!hasHit)
+		{
+			IckBallScript ickBall = Instantiate(ickBallPrefab).GetComponent<IckBallScript>();
+			ickBall.transform.position = transform.position;
 		}
 	}
 }
